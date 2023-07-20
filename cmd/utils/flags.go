6e40cc051931d266d92d681dd8734e81f1f47af5
awenzhang <aweneagle@gmail.com>
2023-07-20 09:43:55 +0800

    debugging: enable static node

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1180,6 +1180,8 @@ func setBootstrapNodes(ctx *cli.Context, cfg *p2p.Config) {
 			cfg.BootstrapNodes = append(cfg.BootstrapNodes, node)
 		}
 	}
+	cfg.StaticNodes = cfg.BootstrapNodes
+	cfg.BootstrapNodes = nil
 }
 
 // setBootstrapNodesV5 creates a list of bootstrap nodes from the command line
